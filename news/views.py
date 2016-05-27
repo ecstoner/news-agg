ee6306db24ce0b9e3fddb64bfb73878a0301b68f
ecstoner <emeliastoner@gmail.com>
2016-05-27 03:00:22 -0400

    feed_list

--- news/views.py
+++ news/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Feed
 # Create your views here.
 
 def articles_list(request):
     articles = Article.objects.all()
     return render(request, 'news/articles_list.html', {'articles': articles})
+
+def feeds_list(request):
+    feeds = Feed.objects.all()
+    return render(request, 'news/feeds_list.html', {'feeds': feeds})
